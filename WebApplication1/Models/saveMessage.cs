ff60ff9ac91394e3a173bd71328b7f8b29f87530
shaikafroz016 <shaikafroz0016@gmail.com>
2023-06-04 06:53:06 +0000

    Added Models

---
+++ WebApplication1/Models/saveMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class saveMessage
+    {
+        public string sender_id { get; set; }
+        public string reciver_id { get; set; }
+        public string Content { get; set; }
+    }
+}
